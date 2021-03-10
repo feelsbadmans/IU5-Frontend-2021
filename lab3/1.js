62146981b1628ff393cb960e021987e47db4b548
feelsbadmans <grysinn@yandex.ru>
2021-03-10 21:36:34 +0300

    Lab 3 complete

--- lab3/1.js
+++ lab3/1.js
@@ -6,7 +6,19 @@
  * 'я Вижу солнце' -> 'Я Вижу Солнце'
  */
 function capitalize(str) {
-    // code here
+    let i = 0;
+    let flag = 1;
+    while (i < str.length) {
+        if (str.charAt(i) === ' ') {
+            flag = 1;
+        }
+        if (flag === 1 && str[i] != ' ') {
+            str = str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1);
+            flag = 0;
+        }
+        i += 1;
+    }
+    return str;
 }
 
 module.exports = capitalize;
